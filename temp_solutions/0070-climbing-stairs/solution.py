bd134fc8d14457b5eb73af4b7528a05fbe8fa1de
Aditya Rao <rao.aditya.codes@gmail.com>
2025-03-15 03:29:52 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (48.93%)

---
+++ temp_solutions/0070-climbing-stairs/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2 
+
+        a, b= 1, 2
+        for _ in range(3, n+1):
+            temp = a + b
+            a = b
+            b=temp
+        return b

8e476e5dfb444a8a312dde0cc5a1b9aa69f223be
Aditya Rao <rao.aditya.codes@gmail.com>
2025-03-25 00:41:08 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.9 MB (32.52%)

--- temp_solutions/0191-number-of-1-bits/solution.py
+++ temp_solutions/0191-number-of-1-bits/solution.py
@@ -2,3 +2,5 @@ class Solution:
     def hammingWeight(self, n: int) -> int:
         binary = bin(n)[2:]
         return (sum(1 for digit in binary if digit == '1'))
+
+       #  "Convert to binary, count the 1s."

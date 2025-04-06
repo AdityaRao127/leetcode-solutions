6f5454252cb2c0c435908bba02280a4c6fe8cfb7
Aditya Rao <rao.aditya.codes@gmail.com>
2025-04-06 07:10:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (52.42%)

--- temp_solutions/0027-remove-element/solution.py
+++ temp_solutions/0027-remove-element/solution.py
@@ -1,18 +1,12 @@
-class Solution(object):
-    def removeElement(self, nums, val):
-        """
-        :type nums: List[int]
-        :type val: int
-        :rtype: int
-        """
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        nums.sort()
 
-        k = 0
-
-        i = 0
-        while i < len(nums):
+        k =0 
+        for i in range(len(nums)):
             if nums[i] != val:
-                k += 1
-                i += 1
-            else:
-                nums.pop(i)
-
+                nums[k] = nums[i]
+                k+=1
+        
+        
+        return k
